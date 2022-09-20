10fa9df987af1ae39e2895765c8fa379de8303f5
HammadSiddique <hammad.siddique01@gmail.com>
2022-09-21 00:31:12 +0500

    Fix linter errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @messages = {greeting: Greeting.all.sample.text}
+    @messages = { greeting: Greeting.all.sample.text }
     render json: @messages
   end
 end
